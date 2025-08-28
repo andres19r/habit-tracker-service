6c754549ef720c3676fdd188e5a6cd00ca10f6ea
Andres Rivero <andres19rivero@gmail.com>
2025-08-27 22:11:38 -0400

    feat(db): set up mongo db instance

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from src import config, app
 
 if __name__ == "__main__":
-    app.run(host=config.HOST, port=config.PORT, debug=config.DEBUG)
+    app.config['HOST'] = config.HOST
+    app.config['DEBUG'] = config.DEBUG
+    app.config['PORT'] = config.PORT
+    app.run()
